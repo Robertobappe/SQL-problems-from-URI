c44dff33907c16db576385440809a972ad37412d
Roberto Silva <90939196+Robertobappe@users.noreply.github.com>
2021-12-07 09:43:02 -0300

    Update expanding_the_business.sql

--- expanding_the_business.sql
+++ expanding_the_business.sql
@@ -1,1 +1,4 @@
+/*The video store company has the objectives of creating several franchises spread throughout Brazil. For this we want to know in which cities our customers live.
+For you to help us select the name of all the cities where the rental company has clients. But please do not repeat the name of the city. */
+
 SELECT city FROM customers GROUP BY city
