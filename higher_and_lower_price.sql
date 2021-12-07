ce85844e5aa5977a566cb9613036e69524d21855
Roberto Silva <90939196+Robertobappe@users.noreply.github.com>
2021-12-07 09:44:09 -0300

    Update higher_and_lower_price.sql

--- higher_and_lower_price.sql
+++ higher_and_lower_price.sql
@@ -1,1 +1,4 @@
+/*The financial sector of our company, wants to know the smaller and higher values of the products, which we sell.
+For this you must display only the highest and lowest price of the products table.*/
+
 SELECT MAX(price), MIN(price) FROM products
