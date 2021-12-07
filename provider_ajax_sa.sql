8b58979c96f3f98006268b6590db671da2a14390
Roberto Silva <90939196+Robertobappe@users.noreply.github.com>
2021-12-07 19:38:13 -0300

    Update provider_ajax_sa.sql

--- provider_ajax_sa.sql
+++ provider_ajax_sa.sql
@@ -1,3 +1,7 @@
+/*The financial sector has encountered some problems in the delivery of one of our providers, the delivery of the products does not match the invoice.
+
+Your job is to display the name of the products and the name of the provider, for the products supplied by the provider 'Ajax SA'.*/
+
 SELECT products.name, providers.name FROM products
 INNER JOIN providers
 ON products.id_providers = providers.id 
