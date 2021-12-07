742ea8015f09d7bc3d567c3f25c9da6f75d1a56e
Roberto Silva <90939196+Robertobappe@users.noreply.github.com>
2021-12-07 20:40:31 -0300

    Update cheap_movies.sql

--- cheap_movies.sql
+++ cheap_movies.sql
@@ -1,3 +1,7 @@
+/*In the past the studio has made an event where several movies were on sale, we want to know what movies these were.
+
+Your job to help us is to select the ID and name of movies whose price is less than 2.00.*/
+
 SELECT movies.id, movies.name FROM movies 
 INNER JOIN prices ON 
 prices.id = movies.id_prices
