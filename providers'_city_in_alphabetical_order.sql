15a553bec87fa048a00e32a1be81cd73a8b4f578
Roberto Silva <90939196+Robertobappe@users.noreply.github.com>
2021-12-07 09:44:57 -0300

    Update providers'_city_in_alphabetical_order.sql

--- providers'_city_in_alphabetical_order.sql
+++ providers'_city_in_alphabetical_order.sql
@@ -1,1 +1,5 @@
+/*Every month the company asks for a report from the cities that providers are registered. So, do a query that returns all the cities of the providers, 
+but in alphabetical order.
+OBS: You must not show repeated cities.*/
+
 SELECT city FROM providers ORDER BY city ASC
