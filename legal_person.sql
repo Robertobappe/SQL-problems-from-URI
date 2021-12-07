49a39dfbf7261fe32c7d1e553706fb704f2fab8b
Roberto Silva <90939196+Robertobappe@users.noreply.github.com>
2021-12-07 14:42:47 -0300

    Update legal_person.sql

--- legal_person.sql
+++ legal_person.sql
@@ -1,3 +1,5 @@
+/*The sales industry wants to make a promotion for all clients that are legal entities. For this you must display the name of the customers that are legal entity.*/
+
 SELECT name FROM customers AS c
 INNER JOIN legal_person AS lp
 ON c.id = lp.id_customers
